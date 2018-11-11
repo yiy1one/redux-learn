a2aa2a190b813f4cff09bf8f2ed4c08b4d45b4c7
yiy1one <yiy1one@163.com>
2018-11-11 14:47:27 +0800

    添加异步方法

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import "antd/dist/antd.css"
-import { Input, Button, List } from 'antd'
 import store from './store'
-import { getInputChangeAction, getAddItemAction, getDeleteItemAction} from './store/actionCreators'
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction, initListAction} from './store/actionCreators'
+import TodoListUI from './TodoListUI'
+import axios from 'axios'
+
 
 class TodoList extends Component {
 
@@ -12,29 +14,27 @@ class TodoList extends Component {
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleStoreChange = this.handleStoreChange.bind(this)
     this.handleButtonClick = this.handleButtonClick.bind(this)
+    this.handleItemDelete = this.handleItemDelete.bind(this)
     store.subscribe(this.handleStoreChange)
   }
 
   render() {
     return (
-      <div style={{marginTop: '10px', marginLeft: '10px'}}>
-        <div>
-          <Input 
-            placeholder="todo info" 
-            value={this.state.inputValue} 
-            style={{width: '300px', marginRight: '10px'}}
-            onChange={this.handleInputChange}
-          />
-          <Button type="primary" onClick={this.handleButtonClick}>提交</Button>
-        </div>
-        <List
-          style={{marginTop: '10px', width: '300px'}}
-          bordered
-          dataSource={this.state.list}
-          renderItem={(item, index) => (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
-        />
-      </div>
+      <TodoListUI 
+        inputValue={this.state.inputValue}
+        handleInputChange={this.handleInputChange}
+        handleButtonClick={this.handleButtonClick}
+        list={this.state.list}
+        handleItemDelete={this.handleItemDelete}
+      />
     )
+  }
+
+  componentDidMount() {
+    axios.get('/list.json').then((res) => {
+      const data = res.data
+      store.dispatch(initListAction(data))
+    })
   }
 
   handleInputChange(e) {
