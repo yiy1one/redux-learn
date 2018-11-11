adb4639af166a322f21f31235cabfe9ce64f57b3
yiy1one <yiy1one@163.com>
2018-11-11 10:59:31 +0800

    添加删除功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -30,7 +30,7 @@ class TodoList extends Component {
           style={{marginTop: '10px', width: '300px'}}
           bordered
           dataSource={this.state.list}
-          renderItem={item => (<List.Item>{item}</List.Item>)}
+          renderItem={(item, index) => (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
         />
       </div>
     )
@@ -51,6 +51,14 @@ class TodoList extends Component {
   handleButtonClick() {
     const action = {
       type: 'add_todo_item'
+    }
+    store.dispatch(action)
+  }
+
+  handleItemDelete(index) {
+    const action = {
+      type: 'delete_todo_item',
+      index
     }
     store.dispatch(action)
   }
